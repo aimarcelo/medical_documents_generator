7897cf95d0010a3abe475c7e6c8b9cd176600350
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-17 01:55:12 +0800

    Add new and index actions to Patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,18 @@
 class PatientsController < ApplicationController
+  def new 
+    @patient = Patient.new
+  end
+  
+  def index
+    @patients = Patient.all
+  end
+ 
   def create
     @patient = user.patients.create(patient_params)
 
     respond_to do |format|
       format.html do
-        redirect_to patient_url(@patient), notice: "Patient was successfully created."
+        redirect_to patients_url(@patient), notice: "Patient was successfully created."
       end
       format.json do
         render json: @patient, status: :ok
@@ -16,7 +24,7 @@ class PatientsController < ApplicationController
 
   def patient_params
     params.require(:patient).permit(
-      :first_name, :last_name, :birthdate, :address, :contact_number 
+      :first_name, :last_name, :birthdate, :contact_number, :city, :province 
     )
   end
 end
