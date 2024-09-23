75637b406f558aab29bef2daa9403b951b99a05b
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-23 18:20:30 +0800

    Modify actions in Patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,11 +4,11 @@ class PatientsController < ApplicationController
   end
   
   def index
-    @patients = Patient.all
+    @patients = Patient.order(:last_name, :first_name)
   end
- 
+
   def create
-    @patient = user.patients.create(patient_params)
+    @patient = Patient.create(patient_params)
 
     respond_to do |format|
       format.html do
@@ -20,11 +20,43 @@ class PatientsController < ApplicationController
     end
   end
 
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    @patient.update(patient_params)
+   
+    respond_to do |format|
+      format.html do
+        redirect_to patient_url(@patient), notice: "Patient was successfully updated."
+      end
+      format.json do
+        render json: @patient, status: "see other"
+      end
+    end
+  end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    @patient.destroy
+
+    respond_to do |format|
+      format.html do
+        redirect_to patients_url, notice: "Patient was successfully deleted."
+      end
+      format.json do
+        render json: @patient, status: "see other"
+      end
+    end
+  end
+ 
   private
 
   def patient_params
     params.require(:patient).permit(
-      :first_name, :last_name, :birthdate, :contact_number, :city, :province 
+      :first_name, :last_name, :middle_name, :birthdate, :email, :contact_number, :city, :province 
     )
   end
 end
