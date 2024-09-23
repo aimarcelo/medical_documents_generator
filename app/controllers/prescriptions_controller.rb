1b68cb5b44589f1f851579067ce72da1e6e8142f
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-23 18:34:07 +0800

    Add actions in Patients controller and add page for new prescription for

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,22 +1,22 @@
 class PrescriptionsController < ApplicationController
-    def create
-      @prescription = user.prescriptions.create(prescription_params)
-  
-      respond_to do |format|
-        format.html do
-          redirect_to prescription_url(@prescription), notice: "Prescription was successfully created."
-        end
-        format.json do
-          render json: @prescription, status: :ok
-        end
+  def create
+    @prescription = Prescription.create(prescription_params)
+
+    respond_to do |format|
+      format.html do
+        redirect_to prescription_url(@prescription), notice: "Prescription was successfully created."
+      end
+      format.json do
+        render json: @prescription, status: :ok
       end
     end
-  
-    private
-  
-    def prescription_params
-      params.require(:prescription).permit(
-        :patient_id, :content
-      )
-    end
+  end
+
+  private
+
+  def prescription_params
+    params.require(:prescription).permit(
+      :patient_id, :content
+    )
+  end
 end
