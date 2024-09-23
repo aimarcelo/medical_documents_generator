49c5cd5710874db1bc2f6f59480cf5f64aa13cd2
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-23 18:22:02 +0800

    Add methods to Patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,17 @@
 class Patient < ApplicationRecord
   has_many :prescriptions, dependent: :destroy
+
+  def full_name 
+    "#{last_name}, #{first_name} " 
+  end
+
+  def city_address
+    "#{city}, #{province}" 
+  end
+
+  def age 
+    now = Time.now.utc.to_date
+    past_birthdate_this_year = now.month > birthdate.month || (now.month == birthdate.month && now.day >= birthdate.day)
+    now.year - birthdate.year - (past_birthdate_this_year ? 0 : 1)
+  end
 end
