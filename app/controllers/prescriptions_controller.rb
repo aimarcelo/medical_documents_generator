9e326eb5868c385346643af6ed7fa75f59d1e5e9
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-25 17:36:16 +0800

    Add actions to prescriptions controller

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class PrescriptionsController < ApplicationController
+  def new
+    @prescription = Prescription.new
+    @patients = Patient.order(:last_name, :first_name)
+  end
+
   def create
     @prescription = Prescription.create(prescription_params)
 
@@ -14,11 +19,47 @@ class PrescriptionsController < ApplicationController
     end
   end
 
+  def index
+    @prescriptions = Prescription.all
+  end
+
+  def edit
+    @prescription = Prescription.find(params[:id])
+  end
+
+  def update
+    @prescription = Prescription.find(params[:id])
+    @prescription.update(prescription_params)
+
+    respond_to do |format|
+      format.html do
+        redirect_to edit_prescription_url(@prescription), notice: 'prescription was successfully updated.'
+      end
+      format.json do
+        render json: @prescription, status: 'see other'
+      end
+    end
+  end
+
+  def destroy
+    @prescription = Prescription.find(params[:id])
+    @prescription.destroy
+
+    respond_to do |format|
+      format.html do
+        redirect_to prescriptions_url, notice: 'Prescription was successfully deleted.'
+      end
+      format.json do
+        render json: @prescription, status: 'see other'
+      end
+    end
+  end
+
   private
 
   def prescription_params
     params.require(:prescription).permit(
-      :patient_id, :content
+      :patient_id, :drug_name, :preparation, :quantity, :dose_and_frequency, :notes
     )
   end
 end
