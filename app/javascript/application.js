19ea2ee2d55f8235af257f10ef4ada8a8c06b594
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-25 17:45:04 +0800

    Install tom-select

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,24 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 import "./components";
+
+import TomSelect from 'tom-select';
+
+// Initialize Tom-select on the dropdown
+document.addEventListener("turbo:load", function() {
+  var patientDropdown = document.querySelector("#patient-dropdown");
+  if (patientDropdown) {
+    new TomSelect(patientDropdown, {
+    create: false,
+    sortField: {
+        field: "text",
+        direction: "asc"
+    },
+    highlight: true,
+    placeholder: "Select a patient...",
+    searchField: ['text'],
+    closeAfterSelect: true,
+    maxOptions: 10
+  });
+  }
+});
