7daf1153c5434cef3883577fa2a6dc351d9f9ef9
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-09-25 17:36:45 +0800

    Add date format methods to Patient model

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -2,4 +2,12 @@
 
 class Prescription < ApplicationRecord
   belongs_to :patient
+
+  def date_created
+    created_at.strftime('%b %d, %Y')
+  end
+
+  def date_updated
+    updated_at.strftime('%b %d, %Y')
+  end
 end
